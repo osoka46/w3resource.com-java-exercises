e0a520124ae25a8f291a4107b5862aa00976fb08
osoka46 <osman.bozdag@yahoo.com>
2022-06-16 23:13:49 +0200

    .

--- Java Programming Exercises, Practice, Solution/src/BasicExercisesPart_I/Question_93/Main.java
+++ Java Programming Exercises, Practice, Solution/src/BasicExercisesPart_I/Question_93/Main.java
@@ -1,8 +1,39 @@
 package BasicExercisesPart_I.Question_93;
 
+import java.util.Scanner;
+
 public class Main {
+    static boolean check(int []arr)
+    {
+        for (int x=0;x< arr.length-1;x++)
+        {
+            if(arr[x]==10&&arr[x+1]==10)
+            {
+                return true;
+            }
+            else if (arr[x]==20&&arr[x+1]==20)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("type size of num. arr");
+
+        int[] arr = new int[input.nextInt()];
+        System.out.println("type arr elements");
+        int size= arr.length;
+        int arrnumbers=0;
+        while (size>0)
+        {
+            arr[arrnumbers]=input.nextInt();
+            arrnumbers++;
+            size--;
+        }
+        System.out.println(check(arr));
 
     }
 }
--- Java Programming Exercises, Practice, Solution/src/BasicExercisesPart_I/Question_94/Main.java
+++ Java Programming Exercises, Practice, Solution/src/BasicExercisesPart_I/Question_94/Main.java
@@ -1,7 +1,32 @@
 package BasicExercisesPart_I.Question_94;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static void check(ArrayList<Integer>arr)
+    {
+        ArrayList<Integer>evenNumbers=new ArrayList<>();
+        ArrayList<Integer>oddNumbers=new ArrayList<>();
+        for(int x=0; x<arr.size();x++)
+        {
+            if (arr.get(x))
+        }
+    }
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("type size of num. arr");
+
+        int[] arr = new int[input.nextInt()];
+        System.out.println("type arr elements");
+        int size= arr.length;
+        int arrnumbers=0;
+        while (size>0)
+        {
+            arr[arrnumbers]=input.nextInt();
+            arrnumbers++;
+            size--;
+        }
 
     }
 }
